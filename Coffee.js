6ac7b47289e8b6a733273c117579c41827945d0b
stakyws <stakywsnablyws@gmail.com>
2025-03-11 04:02:59 +0300

    Design Location Coffee Langoig PHP

---
+++ Coffee.js
@@ -0,0 +1,13 @@
+//swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
